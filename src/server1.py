5f0a25916bb023d5fe6a71c8a47610047d3772bc
John Skilbeck <john.skilbeck@gmail.com>
2020-06-28 00:58:54 -0700

    blah

--- src/server1.py
+++ src/server1.py
@@ -4,27 +4,24 @@ from flask import Flask, Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 # import api as api
 
-app = Flask(__name__, template_folder='views')
-# routes = Blueprint('skilbjo', __name__, template_folder='views/')
-# app.register_blueprint(routes)
+app = Flask(__name__)
+routes = Blueprint('skilbjo', __name__, template_folder='resources/public')
+app.register_blueprint(routes)
+
 
 @app.route('/', methods=['GET'], defaults={'page': 'index'})
-
-@app.route('/hello', methods=['GET'])
-def hello(name=None):
-  page = 'hello'
-  return render_template('{page}.html'.format(page=page))
-
 @app.route('/<page>', methods=['GET'])
 def show(page):
   try:
     render_template('{page}.html'.format(page=page))
   except TemplateNotFound:
     abort(404)
-
+# def index():
+  # return 'Index page'
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+  return '404'
+    # return render_template('404.html'), 404
 
 # @app.route('/user/<user>', methods=['GET'])
 # api.show_user_profile(user)
